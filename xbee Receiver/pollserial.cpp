b296fe80e7caa13b6a9d8008562a100b64c408d4
Shwethamsh <45194829+Shwethamsh@users.noreply.github.com>
2020-05-02 23:39:06 +0530

    Update pollserial.cpp

--- xbee Receiver/pollserial.cpp
+++ xbee Receiver/pollserial.cpp
@@ -1,28 +1,3 @@
-/*
-  pollserial.cpp Heavily modified version of:
-
-  HardwareSerial.cpp - Hardware serial library for Wiring
-  Copyright (c) 2006 Nicholas Zambetti.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  
-  Modified 23 November 2006 by David A. Mellis
-  Modified July 2010 by Myles D. Metzler
-  Modified Aug 2013 by Tony Henrique
-*/
-
 #include <avr/io.h>
 #include <stdlib.h>
 #include "pollserial.h"
