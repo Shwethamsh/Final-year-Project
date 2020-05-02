b3d3e2e1cd248221763105549a48c38442b87f80
Shwethamsh <45194829+Shwethamsh@users.noreply.github.com>
2020-05-02 23:41:14 +0530

    Update xbee Transmiter.ino

--- xbee Transmiter/xbee Transmiter.ino
+++ xbee Transmiter/xbee Transmiter.ino
@@ -1,12 +1,3 @@
-/* 	~ Simple Arduino - xBee Transmitter sketch ~
-
-	Read an analog value from potentiometer, then convert it to PWM and finally send it through serial port to xBee.
-	The xBee serial module will send it to another xBee (resiver) and an Arduino will turn on (fade) an LED.
-	The sending message starts with '<' and closes with '>' symbol. 
-	
-	Dev: Michalis Vasilakis // Date:2/3/2016 // Info: www.ardumotive.com // Licence: CC BY-NC-SA                    */
-
-//Constants: 
 const int potPin = A0; //Pot at Arduino A0 pin 
 //Variables:
 int value ; //Value from pot
